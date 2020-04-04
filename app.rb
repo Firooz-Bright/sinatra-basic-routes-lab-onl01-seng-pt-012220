cdd9cd0657f66e6b3c6574aedc58b62b8395ee11
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-04-04 13:04:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get'/name' do
-    
+    "My name is William Workman"
   end 
   
   get'/hometown' do 
-    
+    "My hometown is San Francisco"
   end 
   
   get '/favorite-song' do 
